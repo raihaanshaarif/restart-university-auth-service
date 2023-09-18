513d906ad81592cc143686fd05434f25b67500dd
raihaanshaarif <codexbd@gmail.com>
2023-09-18 18:59:48 +0600

    [Done] Logger Setup Complete 12.7

---
+++ src/shared/logger.ts
@@ -0,0 +1,56 @@
+import { createLogger, format, transports } from 'winston'
+import path from 'path'
+import DailyRotateFile from 'winston-daily-rotate-file'
+
+const { combine, timestamp, label, printf } = format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+  return `${date.toString()} ${hour}:${minute}:${second} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'sueccess',
+        '%DATE%-success.log',
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+const errorlogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'right meow!' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        '%DATE%-error.log',
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+export { logger, errorlogger }
