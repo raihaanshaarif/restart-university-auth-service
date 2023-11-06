9be63e2416417d41179c05a487ab10c087d5dfdc
raihaanshaarif <codexbd@gmail.com>
2023-11-06 18:10:58 +0600

    [Done] Page and limit of pagination complmete

--- src/app/interfaces/common.ts
+++ src/app/interfaces/common.ts
@@ -5,3 +5,12 @@ export type IGenericErrorResponse = {
   message: string;
   errorMessages: iGenericErrorMessage[];
 };
+
+export type IGenericResponse<T> = {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data: T;
+};
--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,6 +3,9 @@ import { AcademicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
+import { IAcademicSemester } from './academicSemester.interface';
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -10,7 +13,7 @@ const createSemester = catchAsync(
     const result =
       await AcademicSemesterService.createSemester(academicSemesterData);
 
-    sendResponse(res, {
+    sendResponse<IAcademicSemester>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Academic Semester Created Successfully',
@@ -19,7 +22,27 @@ const createSemester = catchAsync(
     next();
   },
 );
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paginationOptions = pick(req.query, paginationFields);
+
+    // console.log(paginationOptions);
+
+    const result =
+      await AcademicSemesterService.getAllSemesters(paginationOptions);
+
+    sendResponse<IAcademicSemester[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semesters Retrived successfilly',
+      meta: result.meta,
+      data: result.data,
+    });
+    next();
+  },
+);
 
 export const AcademicSemesterController = {
   createSemester,
+  getAllSemesters,
 };
